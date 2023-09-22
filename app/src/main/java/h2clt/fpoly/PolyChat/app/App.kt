861581823d2f83ac62d60dd919644a847f1762b8
long213k@gmail.com <109726982+Bayboibad@users.noreply.github.com>
2023-09-22 17:34:54 +0700

    Longboiz

--- app/src/main/java/h2clt/fpoly/PolyChat/app/App.kt
+++ app/src/main/java/h2clt/fpoly/PolyChat/app/App.kt
@@ -2,15 +2,13 @@
 package h2clt.fpoly.PolyChat.app
 
 import androidx.compose.animation.Crossfade
-
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-
-import com.yourcompany.android.jetpackcompose.router.Router
-import com.yourcompany.android.jetpackcompose.router.Screen
+import h2clt.fpoly.PolyChat.router.Router
+import h2clt.fpoly.PolyChat.router.Screen
 import h2clt.fpoly.PolyChat.screens.HomeScreen
 import h2clt.fpoly.PolyChat.screens.LoginScreen
+import h2clt.fpoly.PolyChat.screens.ProfileScreen
 import h2clt.fpoly.PolyChat.ui.theme.Purple80
 import thanhltph29166.fpt.baithi.test2.Screen.MoveTextAnimation
 
@@ -23,6 +21,7 @@ fun App() {
         is Screen.LoginScreen -> LoginScreen()
         is Screen.HomeScreen -> HomeScreen()
         is Screen.RemindUserScreen -> MoveTextAnimation()
+        is Screen.ProfileScreen -> ProfileScreen()
 
 
         else -> {}
--- app/src/main/java/h2clt/fpoly/PolyChat/router/Router.kt
+++ app/src/main/java/h2clt/fpoly/PolyChat/router/Router.kt
@@ -6,7 +6,9 @@ import androidx.compose.runtime.mutableStateOf
 sealed class Screen {
    object Navigation : Screen()
    object LoginScreen : Screen()
+    object RemindUserScreen : Screen()
    object HomeScreen : Screen()
+    object ProfileScreen : Screen()
    object Buttons : Screen()
    object ProgressIndicator : Screen()
    object AlertDialog : Screen()
@@ -17,7 +19,7 @@ sealed class Screen {
 }
 
 object Router {
-  var currentScreen: MutableState<Screen> = mutableStateOf(Screen.HomeScreen)
+  var currentScreen: MutableState<Screen> = mutableStateOf(Screen.ProfileScreen)
 
   fun navigateTo(destination: Screen) {
     currentScreen.value = destination
